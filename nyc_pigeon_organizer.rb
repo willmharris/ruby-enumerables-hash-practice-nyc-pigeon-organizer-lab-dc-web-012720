74dd4a6dcd3c47eb151403fadd6a22a201acf61c
William Harris <willmharris0@gmail.com>
2020-01-26 23:47:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+ 
 end
+
+ # I want to create a hash of hashes of arrays 
+  # My mid-level hashes are :color : gender :lives 
+  # I want to create a key for every unique bird name in the data 
+  # I want to assign a value to that key of 
+    # :color => [appropriate color]
+    # :gender => [appropriate gender]
+    # :lives => [appropriate location]
+
+
+end 
