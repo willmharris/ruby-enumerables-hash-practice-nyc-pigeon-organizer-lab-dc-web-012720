154420b19c665397f38cfc9a8e32ff9c40d352ca
William Harris <willmharris0@gmail.com>
2020-01-26 23:59:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,11 +10,14 @@ end
   unique_bird_name_array = ["bird 1", "bird2"]
   i = 0 
   while i < unique_bird_name_array.length 
-    current_bird = unique_bird_name_array[i]
-    finalhash[current_bird] = {
-      :color => appropriate array,
-      :gender => appropriate array,
-      :lives => appropriate array 
+    current_bird_name = unique_bird_name_array[i]
+    correct_colors_array = # code 
+    correct_gender_array = # code 
+    correct_lives_array = # code 
+    finalhash[current_bird_name] = {
+      :color => correct_colors_array,
+      :gender => correct_gender_array,
+      :lives => correct_lives_array 
     }
     i += 1 
   end 
