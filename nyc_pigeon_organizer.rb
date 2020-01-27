552a6acf142f7c18466ade83e4e890cac4dc8773
William Harris <willmharris0@gmail.com>
2020-01-27 00:34:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,11 +2,7 @@ def nyc_pigeon_organizer(data)
  
 end
 
-# Get an array of unique pigeon names 
-# Map that array to an empty hash (filled in with appropriate subhashes) with the names as keys 
-# Fill in each empty hash appropriately 
 
-#  array of unique bird names 
   unique_bird_name_array = ["bird 1", "bird2"]
   i = 0 
   finalhash = [] 
